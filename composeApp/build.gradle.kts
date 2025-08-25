7cd86e978bc02a3bcfbeb0be00bda6fef1e3e7ea
Maicol Battistini <maicolbattistini@live.it>
2025-08-25 21:14:37 +0200

    feat: 🎉 Initial commit

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose)
+    id("dev.opensavvy.vite.kotlin") version "0.6.0"
+}
+
+kotlin {
+    js {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.ui)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
+        }
+
+    }
+}
